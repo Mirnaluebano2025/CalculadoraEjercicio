8aab43979758bf53954282e84f63ddc684bc6ae4
Mirnaluebano2025 <mirnaluebano@gmail.com>
2025-03-11 17:11:47 -0600

    Se agrego la funcionalidad de la calculadora 2.0 para validar solo numeros y no letras

--- calculadora2.0/codigo.js
+++ calculadora2.0/codigo.js
@@ -1,38 +1,52 @@
 function startCalculadora() {
     const sumar = (num1, num2) => {
-        return parseFloat(num1) + parseFloat(num2);
+        return (num1) + (num2);
     }
     const restar = (num1, num2) => {
-        return parseFloat(num1) - parseFloat(num2);
+        return (num1) - (num2);
     }
     const dividir = (num1, num2) => {
-        return parseFloat(num1) / parseFloat(num2);
+        return (num2 !== 0 ? num1 / num2 : "Error: División por cero");
     }
     const multiplicar = (num1, num2) => {
-        return parseFloat(num1) * parseFloat(num2);
+        return (num1) * (num2);
+    }
+          
+    function pedirNumero(mensaje) {
+        let numero;
+        do {
+            numero = prompt(mensaje);
+            if (numero === null) return null;
+            numero = Number(numero);
+            if (isNaN(numero)) {
+                alert("Debes ingresar un número válido");
+            }
+        } while (isNaN(numero));
+        return numero;
     }
     
     alert ("Que operación deseas realizar?");
     let operacion = prompt ("1. Sumar, 2. Restar, 3. Dividir, 4. Multiplicar");
+
     
     if (operacion ==1) {
-        let num1 = prompt ("Ingresa el primer número para sumar");
-        let num2 = prompt ("Ingresa el segundo número para sumar");
+        num1 = pedirNumero ("Ingresa el primer número para sumar");
+        num2 = pedirNumero ("Ingresa el segundo número para sumar");
         alert ("El resultado de la suma es: " + sumar(num1, num2));
     }
     else if (operacion ==2) {
-        let num1 = prompt ("Ingresa el primer número para restar");
-        let num2 = prompt ("Ingresa el segundo número para restar");
+        num1 = pedirNumero ("Ingresa el primer número para restar");
+        num2 = pedirNumero ("Ingresa el segundo número para restar");
         alert ("El resultado de la resta es: " + restar(num1, num2));
     }
     else if (operacion ==3) {
-        let num1 = prompt ("Ingresa el primer número para dividir");
-        let num2 = prompt ("Ingresa el segundo número para dividir");
+        num1 = pedirNumero ("Ingresa el primer número para dividir");
+        num2 = pedirNumero ("Ingresa el segundo número para dividir");
         alert ("El resultado de la división es: " + dividir(num1, num2));
     }
     else if (operacion ==4) {
-        let num1 = prompt ("Ingresa el primer número para multiplicar");
-        let num2 = prompt ("Ingresa el segundo número para multiplicar");
+        num1 = pedirNumero ("Ingresa el primer número para multiplicar");
+        num2 = pedirNumero ("Ingresa el segundo número para multiplicar");
         alert ("El resultado de la multiplicación es: " + multiplicar(num1, num2));
     }
     else {
